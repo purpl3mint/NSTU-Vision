8ddfb573e129017a56c43a6e955f86ab3b7c20a7
Dmitry <purpl3mint@yandex.ru>
2021-01-27 21:47:05 +0700

    Edited production paths

--- server.js
+++ server.js
@@ -17,14 +17,17 @@ app.use('/api/signal', require('./routes/signal.routes'));
 app.use('/api/video', require('./routes/video.routes'));
 
 if (process.env.NODE_ENV === 'production') {
+    /*
     app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
-else {
-
+    */
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + 'client/build/index.html'));
+    });
 }
 
 const PORT = process.env.PORT || config.get('port') || 5000;
